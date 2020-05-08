b6276eb37c82a97c1d0736e56de215db4c99a171
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2020-05-08 22:20:03 +0700

    Added tooltip

--- src/components/Choropleth.js
+++ src/components/Choropleth.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { scaleLinear } from "d3-scale";
+import ReactTooltip from "react-tooltip";
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import { toTitleCase } from '../helper';
 import positiveData from '../data/data_kecamatan_positif.json';
 import topoMap from '../topojson/bandung.json';
 
@@ -8,6 +10,8 @@ export class Choropleth extends React.Component {
   state = {
     data: null,
     maxPercentage: 0,
+    showTooltip: false,
+    activeData: null,
   };
 
   initData = () => {
@@ -24,7 +28,11 @@ export class Choropleth extends React.Component {
         maxPercentage = percentage;
       }
       data[name] = {
+        name: toTitleCase(d.Kecamatan),
         amount: d.Positif,
+        bedrest: d.Dirawat,
+        cured: d.Sembuh,
+        dead: d.Meninggal,
         percentage,
       };
     });
@@ -32,40 +40,75 @@ export class Choropleth extends React.Component {
     this.setState({data, maxPercentage});
   }
 
+  setActiveData = name => {
+    this.setState({
+      showTooltip: true,
+      activeData: this.state.data[name]
+    });
+  }
+
+  renderToooltip = () => {
+    const {activeData} = this.state;
+    return (
+      <ReactTooltip>
+        {this.state.showTooltip ? (
+          <div className="tooltip">
+            <p>{activeData.name}</p>
+            <p>Positif: {activeData.amount}</p>
+            <p>Dirawat: {activeData.bedrest}</p>
+            <p>Sembuh: {activeData.cured}</p>
+            <p>Meninggal: {activeData.dead}</p>
+          </div>
+        ) : null}
+      </ReactTooltip>
+    );
+  }
+
   renderMap = () => {
+    const maxWidth = window.innerWidth;
     const colorScale = scaleLinear()
       .domain([0, this.state.maxPercentage])
       .range(["#ffa899", "#ff2700"]);
 
     return (
-      <ComposableMap
-        projectionConfig={{
-          scale: 120000,
-          center: [107.65, -6.95]
-        }}>
-        <Geographies geography={topoMap}>
-          {({ geographies }) =>
-            geographies.map(geo => {
-              let geoName = geo.properties.KECAMATAN.split(' ').join('');
-              let geoData = this.state.data[geoName];
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={colorScale(geoData.percentage)}
-                  strokeWidth={0.25}
-                  stroke={'#fff'}
-                  style={{
-                    hover: {
-                      fill: '#F53'
-                    }
-                  }}
-                />
-              )
-            })
-          }
-        </Geographies>
-      </ComposableMap>
+      <>
+        {this.renderToooltip()}
+        <ComposableMap
+          data-tip=""
+          width={maxWidth / 2.2}
+          projection="geoMercator"
+          projectionConfig={{
+            scale: 120000,
+            center: [107.65, -6.95]
+          }}>
+          <Geographies geography={topoMap}>
+            {({ geographies }) =>
+              geographies.map(geo => {
+                let geoName = geo.properties.KECAMATAN.split(' ').join('');
+                let geoData = this.state.data[geoName];
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={colorScale(geoData.percentage)}
+                    strokeWidth={0.25}
+                    stroke={'#fff'}
+                    onMouseEnter={() => this.setActiveData(geoName)}
+                    onMouseLeave={() => {
+                      this.setState({showTooltip: false, activeData: null});
+                    }}
+                    style={{
+                      hover: {
+                        fill: '#F53'
+                      }
+                    }}
+                  />
+                )
+              })
+            }
+          </Geographies>
+        </ComposableMap>
+      </>
     )
   }
 
